7203078906d1b94e162a8e6cd7a428190fdcebc8
Amirhossein-Ra <000amir111rahzany@gmail.com>
2024-09-23 21:34:28 +0330

    Create the header and image of posts

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 export default function Item({ id, userName, userImage, image, caption }) {
   return (
-    <div>
-      <h1>{userName}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImage}
+          alt={userName}
+        />
+        <p className="font-bold flex-1">{userName}</p>
+        <HiOutlineDotsHorizontal size={20} />
+      </div>
+
+      <img className="object-cover w-full" src={image} alt="" />
     </div>
   );
 }
