04e488e46d8712d6951968bc0e13b607251c16f4
Amirhossein-Ra <000amir111rahzany@gmail.com>
2024-09-26 14:43:09 +0330

    adding buttons for post

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { IoChatbubbleOutline } from "react-icons/io5";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { CiBookmark } from "react-icons/ci";
 export default function Item({ id, userName, userImage, image, caption }) {
   return (
     <div className="bg-white my-7 border rounded-md">
@@ -14,6 +17,14 @@ export default function Item({ id, userName, userImage, image, caption }) {
       </div>
 
       <img className="object-cover w-full" src={image} alt="" />
+
+      <div className="flex items-center justify-between">
+        <div className="flex space-x-4">
+          <AiOutlineHeart className="btn" />
+          <IoChatbubbleOutline className="btn" />
+        </div>
+        <CiBookmark className="btn" />
+      </div>
     </div>
   );
 }
