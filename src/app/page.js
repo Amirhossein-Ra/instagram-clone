e23adde860f4b49288ca3dd421a0f277cc493af0
Amirhossein-Ra <000amir111rahzany@gmail.com>
2024-09-28 15:32:03 +0330

    Make Feed section responsive

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="p-3 bg-gray-50 min-h-screen">
+    <div className=" bg-gray-50 min-h-screen">
       <Header />
       <Feed />
     </div>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,12 +5,12 @@ import Posts from "./Post";
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2 ">
         <Stories />
         <Posts />
       </section>
-      <section></section>
+      <section className=" hidden md:inline-grid md:grid-span-1 "></section>
     </main>
   );
 }
